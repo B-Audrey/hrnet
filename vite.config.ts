b48c73b9180d4eb3b540db58b6690ef253209290
audrey <audrey@devzone.fr>
2024-08-02 18:58:29 +0200

    feat: remove style import from lib

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import sass from 'sass'
 
 export default defineConfig({
-  base: '/',
+  base: '/hrnet',
   plugins: [react()],
   css: {
     preprocessorOptions: {
