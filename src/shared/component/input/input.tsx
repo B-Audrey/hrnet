7e019166bbfbbbdc508132eebbbd8c6c6abdfaf7
audrey <audrey@devzone.fr>
2024-08-05 10:45:46 +0200

    fix: prettier

--- src/shared/component/input/input.tsx
+++ src/shared/component/input/input.tsx
@@ -1,49 +1,48 @@
 import './input.scss';
-import React, {useState} from 'react';
-import {InputProps} from '../../interface/props.interface.ts';
-import {isNumberValid} from '../../utils/numberValidator.ts';
-import {isStringValid} from '../../utils/stringValidator.ts';
+import React, { useState } from 'react';
+import { InputProps } from '../../interface/props.interface.ts';
+import { isNumberValid } from '../../utils/numberValidator.ts';
+import { isStringValid } from '../../utils/stringValidator.ts';
 
-const Input = ({label, name, id, type, isRequired, validatorOptions, min, max, setValue}: InputProps) => {
-    const [isValueValid, setIsValueValid] = useState({isValid: true, message: ''});
+const Input = ({ label, name, id, type, isRequired, validatorOptions, min, max, setValue }: InputProps) => {
+  const [isValueValid, setIsValueValid] = useState({ isValid: true, message: '' });
 
-    /**
-     * Handle the blur event on the input
-     * Check if the input is valid or not to send error if needed
-     * @param e
-     */
-    const errorControlBlurFn = (e: React.FocusEvent<HTMLInputElement>) => {
-        if (type === 'text') {
-            const isInputInError = isStringValid(e.target.value, validatorOptions!);
-            return setIsValueValid(isInputInError);
-        }
-        if (type === 'number') {
-            const isInputInError = isNumberValid(Number(e.target.value), min, max!);
-            return setIsValueValid(isInputInError);
-        }
+  /**
+   * Handle the blur event on the input
+   * Check if the input is valid or not to send error if needed
+   * @param e
+   */
+  const errorControlBlurFn = (e: React.FocusEvent<HTMLInputElement>) => {
+    if (type === 'text') {
+      const isInputInError = isStringValid(e.target.value, validatorOptions!);
+      return setIsValueValid(isInputInError);
     }
+    if (type === 'number') {
+      const isInputInError = isNumberValid(Number(e.target.value), min, max!);
+      return setIsValueValid(isInputInError);
+    }
+  };
 
-    return (
-        <div className="input-container">
-            <label className='labels' htmlFor={id}>{label}</label>
-            <input
-                autoComplete={"on"}
-                className='input-content'
-                required={isRequired}
-                type={type}
-                id={id}
-                name={name}
-                onBlur={(e) => errorControlBlurFn(e)}
-                min={min && min}
-                max={max && max}
-                onChange={(e) => setValue && setValue(e.target.value) }
-            />
-            <span className={`input-error ${!isValueValid.isValid ? 'fade-in' : ''}`}>
-                    {isValueValid.message}
-                </span>
-
-        </div>
-    )
+  return (
+    <div className="input-container">
+      <label className="labels" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        autoComplete={'on'}
+        className="input-content"
+        required={isRequired}
+        type={type}
+        id={id}
+        name={name}
+        onBlur={e => errorControlBlurFn(e)}
+        min={min && min}
+        max={max && max}
+        onChange={e => setValue && setValue(e.target.value)}
+      />
+      <span className={`input-error ${!isValueValid.isValid ? 'fade-in' : ''}`}>{isValueValid.message}</span>
+    </div>
+  );
 };
 
 export default Input;
--- src/shared/component/table/table-container.tsx
+++ src/shared/component/table/table-container.tsx
@@ -1,102 +1,96 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
-import {Paper, TablePagination} from '@mui/material';
-import {Employee} from '../../interface/employee.interface.ts';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { Paper, TablePagination } from '@mui/material';
+import { Employee } from '../../interface/employee.interface.ts';
 import Input from '../input/input.tsx';
 import useEmployeeData from '../../hook/useEmployeeDataService.tsx';
 import TableContent from './table-content.tsx';
 import Loader from '../loader/loader.tsx';
 
-
 const DataTableContainer = () => {
-    const [currentPage, setCurrentPage] = useState<number>(0);
-    const [rowsPerPage, setRowsPerPage] = useState<number>(10);
-    const [filterToSearchOn, setFilterToSearchOn] = useState<string>('');
-    const [order, setOrder] = useState<'asc' | 'desc'>('asc');
-    const [orderBy, setOrderBy] = useState<keyof Employee>('id');
-    const [data, setData] = useState<{ employees: Employee[], totalCount: number }>({employees: [], totalCount: 0});
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+  const [filterToSearchOn, setFilterToSearchOn] = useState<string>('');
+  const [order, setOrder] = useState<'asc' | 'desc'>('asc');
+  const [orderBy, setOrderBy] = useState<keyof Employee>('id');
+  const [data, setData] = useState<{ employees: Employee[]; totalCount: number }>({ employees: [], totalCount: 0 });
 
-    const {getData, loading} = useEmployeeData();
+  const { getData, loading } = useEmployeeData();
 
-    /**
-     * Fetch data when the component mounts and when params changes
-     */
-    useEffect(() => {
-        getData({filterToSearchOn, order, orderBy, currentPage, rowsPerPage}).then((data) => data && setData(data));
-    }, [currentPage, rowsPerPage, filterToSearchOn, order, orderBy]);
+  /**
+   * Fetch data when the component mounts and when params changes
+   */
+  useEffect(() => {
+    getData({ filterToSearchOn, order, orderBy, currentPage, rowsPerPage }).then(data => data && setData(data));
+  }, [currentPage, rowsPerPage, filterToSearchOn, order, orderBy]);
 
-    /**
-     * Handle page change to update local state
-     * @param _event
-     * @param newPage
-     */
-    const handleChangePage = (_event: any, newPage: number) => {
-        setCurrentPage(newPage);
-    };
+  /**
+   * Handle page change to update local state
+   * @param _event
+   * @param newPage
+   */
+  const handleChangePage = (_event: any, newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
-    /**
-     * Handle rows per page change to update local state
-     * @param event
-     */
-    const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value));
-        setCurrentPage(0);
-    };
+  /**
+   * Handle rows per page change to update local state
+   * @param event
+   */
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value));
+    setCurrentPage(0);
+  };
 
-    /**
-     * Handle request sort to update local state
-     * @param propertyToOrderBy
-     */
-    const handleRequestSort = (propertyToOrderBy: keyof Employee) => {
-        const isAsc = orderBy === propertyToOrderBy && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(propertyToOrderBy);
-    };
+  /**
+   * Handle request sort to update local state
+   * @param propertyToOrderBy
+   */
+  const handleRequestSort = (propertyToOrderBy: keyof Employee) => {
+    const isAsc = orderBy === propertyToOrderBy && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(propertyToOrderBy);
+  };
 
-    /**
-     * Handle filter change to update local state when the inout value change
-     * Set current page to 0 to re-start from the beginning
-     * @param value
-     */
-    const handleFilterChange = (value: string) => {
-        setFilterToSearchOn(value);
-        setCurrentPage(0);
-    };
+  /**
+   * Handle filter change to update local state when the inout value change
+   * Set current page to 0 to re-start from the beginning
+   * @param value
+   */
+  const handleFilterChange = (value: string) => {
+    setFilterToSearchOn(value);
+    setCurrentPage(0);
+  };
 
-
-    return (
-        <div id={'table'}>
-            <Input label={'Search on table'}
-                   name={'filterToSearchOn'}
-                   id={'search-input'}
-                   type={'text'}
-                   isRequired={false}
-                   setValue={handleFilterChange}
+  return (
+    <div id={'table'}>
+      <Input
+        label={'Search on table'}
+        name={'filterToSearchOn'}
+        id={'search-input'}
+        type={'text'}
+        isRequired={false}
+        setValue={handleFilterChange}
+      />
+      <Paper>
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <TableContent data={data.employees} order={order} orderBy={orderBy} handleRequestSort={handleRequestSort} />
+            <TablePagination
+              className={'table-pagination'}
+              component="div"
+              count={data.totalCount}
+              page={currentPage}
+              onPageChange={handleChangePage}
+              rowsPerPage={rowsPerPage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
             />
-            <Paper>
-                {loading ? (
-                    <Loader />
-                ) : (
-                    <>
-                        <TableContent
-                            data={data.employees}
-                            order={order}
-                            orderBy={orderBy}
-                            handleRequestSort={handleRequestSort}
-                        />
-                        <TablePagination
-                            className={'table-pagination'}
-                            component="div"
-                            count={data.totalCount}
-                            page={currentPage}
-                            onPageChange={handleChangePage}
-                            rowsPerPage={rowsPerPage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                        />
-                    </>
-                )}
-            </Paper>
-        </div>
-    );
+          </>
+        )}
+      </Paper>
+    </div>
+  );
 };
 
 export default DataTableContainer;
